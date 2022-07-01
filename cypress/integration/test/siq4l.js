7a0378a6cbe57206c86f1b5bdec1c0c793dd4d7f
laxman7777 <laxman.binawade@gmail.com>
2022-07-01 11:14:25 +0530

    testatlasurl

--- cypress/integration/test/siq4l.js
+++ cypress/integration/test/siq4l.js
@@ -4,8 +4,9 @@ describe("My siq4l suite test case execution",function()
     it("My first test case execution from siq4l suite",function()
     {
         cy.log("test case execution started")
-        cy.visit("http://10.5.0.9:9080/ilg-fulfillment-mock/FulfillmentService")
+       // cy.visit("http://10.5.0.9:9080/ilg-fulfillment-mock/FulfillmentService")
        // cy.title().should('eq', 'BWS DEV Instance') //expected BWS DEV Instance to equal My SIQ4L Application
+       cy.visit("http://10.5.0.9:9094/PolicyAtlas")
 
     })
 })
